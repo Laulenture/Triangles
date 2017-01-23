182b6d35911cb5bb4251e0aa5eb43537c73bcbc3
LAULENOS-PORTAB\Hikaru <l.aulen@ludus-academie.com>
2017-01-23 17:19:17 +0100

    no message

---
+++ triangle.pas
@@ -0,0 +1,57 @@
+﻿program triangle;
+
+uses crt;
+
+{
+//BUT : Fait un triangle
+//ENTREE : La taille du triangle
+//SORTIE : un triangle de la taille désirée
+
+VAR
+	i, j, taille : ENTIER
+	
+DEBUT
+	ECRIRE 'Entrez la taille du triangle'
+	LIRE taille
+	POUR i DE 1 A taille - 1 FAIRE
+	DEBUT
+		POUR j DE 1 A taille FAIRE
+			SI ((j = 1) OU (j = i)) ALORS
+				ECRIRE('X')
+			SINON
+				SI j < i ALORS ECRIRE('O')
+			FINSI
+		FINPOUR
+		ECRIRE()
+	FINPOUR
+	POUR i DE 1 A taille FAIRE ECRIRE('X')
+FIN
+}
+
+{CONST
+	taille = 8;}
+
+VAR
+	i, j, taille : INTEGER;
+	
+BEGIN
+	clrscr;
+	writeln('Entrez une taille');
+	readln(taille);								//Récupère la taille du triangle
+	FOR i := 1 TO (taille - 1) DO				//Boucle pour écrire les lignes à l'exeption de la dernière
+	BEGIN
+		FOR j := 1 TO taille DO					//Boucle pour écrire le contenu de la ligne
+		BEGIN
+			IF ((j = 1) OR (j = i)) THEN		//Si le caractère écris est le premier ou le dernier de la ligne il sera un X
+				write('X')
+			ELSE
+			BEGIN
+				IF j < i THEN write('O');		//Si la caractère écrit est entre le premier et le dernier il sera un O
+			END;
+		END;
+		writeln();								//Passage à la ligne suivante
+	END;
+	FOR i := 1 TO taille DO						//Boucle pour écrire la dernière ligne
+	write('X');
+	readln();
+END.
---
+++ triangle2.pas
@@ -0,0 +1,43 @@
+﻿program triangle2;
+
+uses crt;
+
+{
+//BUT : Fait un triangle2
+//ENTREE : La taille du triangle2
+//SORTIE : Un triangle de la taille désirée
+
+VAR
+	taille, i, j : ENTIER
+	
+DEBUT
+	ECRIRE 'Entrez la taille du triangle'		
+	LIRE taille									//Récupération de la taille du triangle
+	POUR i DE 1 A taille FAIRE					//Boucle pour ecrire les ligne (diminue la longeur de la ligne à chaque itération)
+		POUR j de 1 A (taille - i + 1) FAIRE	//Bouble qui écrite le contenu de la ligne (diminue la longeur de la ligne à chaque itération)
+			ECRIRE(i-1)							//Le chiffre écrit est i - 1 pour commencer à 0 au lieu de 1
+		FINPOUR
+	FINPOUR
+FIN
+}
+
+{CONST
+	taille = 10;}
+
+VAR	
+	taille, i, j : INTEGER;
+	
+BEGIN
+	clrscr;
+	writeln('Entrez la taille du triangle');
+	readln(taille);
+	FOR i := 1 TO taille DO
+	BEGIN
+		FOR j := 1 TO (taille -i + 1) DO
+		BEGIN
+			write(i-1);
+		END;
+	writeln();
+	END;
+	readln();
+END.
